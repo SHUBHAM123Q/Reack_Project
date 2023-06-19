d2a4da60901007d9f1edfe3e4d1864a7bf3064fb
shubham <shubhamrabadiya4950@gmail.com>
2023-06-19 08:00:57 +0530

    Multipale Filterdata

--- src/Componets/Filltering Data/Filter1.jsx
+++ src/Componets/Filltering Data/Filter1.jsx
@@ -4,20 +4,15 @@ const Filter1 = (props) => {
     let {author,country,language,pages,title,year,img} = props;
   return (
     <>
-      <div className='inline-block overflow-auto ml-5 mb-1 hover:bg-purple-300 mt-4 bg-pink-400 shadow-2xl border-2 border-black w-[350px]'>
-        <div className='p-2 '>
-          <img src={img} alt='NOt Show' className='w-[150px] m-auto'/>
-        </div>
-        <div className='text-center'>
-          <p className=''>Author:{author}</p>
-          <p className=''>Country:{country}</p>
-          <p className=''>Language:{language}</p>
-          <p className=''>Pages:{pages}</p>
-          <p className=''>Title:{title}</p>
-          <p className=''>Year:{year}</p>
-        </div>
-      </div>
-      
+        <tr className='border-2 border-black'>
+          <td><img src={img} alt='NOt Show' className='w-[150px] m-auto overflow-hidden h-[150px] border-black'/></td>
+          <td><p className='text-center'>{author}</p></td>
+          <td><p className='text-center'>{country}</p></td>
+          <td><p className='text-center'>{language}</p></td>
+          <td><p className='text-center'>{pages}</p></td>
+          <td><p className='text-center'>{title}</p></td>
+          <td><p className='text-center'>{year}</p></td>
+        </tr>
     </>
   )
 }
--- src/Componets/Filltering Data/Filterdata.jsx
+++ src/Componets/Filltering Data/Filterdata.jsx
@@ -2,20 +2,41 @@ import React from 'react'
 import Filters from './Filters.json'
 import Filter1 from './Filter1';
 
-const Filter = () => {
-    var from ="Dr Sreeram Chauliahas";
+const Filterdata = () => {
+    var from =["Dr Sreeram Chauliahas","Vinit Karnik"];
   return (
     <>
-        {
-            Filters.filter((ele)=>{return from === ele.author}).map((ele)=>{
-                return(
-                    <Filter1 author={ele.author} country={ele.country} language={ele.language} pages={ele.pages} title={ele.title} year={ele.year} img={ele.img}/>
-                )
-            })
-        }
-               
+        <table>
+            <thead className='border-2 border-black'>
+                <th className='w-[150px] bg-blue-500 text-center border-2 '>Img</th>
+                <th className='w-[250px] bg-red-500 text-center border-2'>Author</th>
+                <th className='w-[150px] bg-blue-500 text-center border-2'>Country</th>
+                <th className='w-[150px] bg-green-500 text-center border-2'>Language</th>
+                <th className='w-[100px] bg-purple-300 text-center border-2'>Pages</th>
+                <th className='w-[250px] bg-green-500 text-center border-2'>Title</th>
+                <th className='w-[50px] bg-red-500 text-center border-2'>Year</th>
+            </thead>
+    
+
+            <tbody >
+                {
+                    Filters.filter((ele)=>{return from[0] === ele.author}).map((ele)=>{
+                        return(
+                            <Filter1 author={ele.author} country={ele.country} language={ele.language} pages={ele.pages} title= {ele.title} year={ele.year} img={ele.img}/>
+                        )
+                    })
+                } 
+            </tbody>
+            <br/>
+            {
+                Filters.filter((ele)=>{return from[1] === ele.author}).map((ele)=>{
+                    return(
+                        <Filter1 author={ele.author} country={ele.country} language={ele.language} pages={ele.pages} title={ele.title} year={ele.year} img={ele.img} />
+                    )
+                })
+            }
+        </table>
     </>
   )
 }
-
-export default Filter
+export default Filterdata
