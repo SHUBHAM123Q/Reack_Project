9efcffe8f4bee2aabfc90d1f784ec5f1c7459733
shubham <shubhamrabadiya4950@gmail.com>
2023-06-30 16:26:56 +0530

    Input Edit

--- src/Componets/Table/product.jsx
+++ src/Componets/Table/product.jsx
@@ -7,16 +7,54 @@ const Product = () => {
 
     const handleClick = () => {
         setproductdata([]);
-
     }
 
     const handleRemove = (productId) => {
         setproductdata(nproductdata.filter(product => product.id !== productId))
-
     }
 
+    let [name,setName]=useState("");
+    let [email,setEmail]=useState("");
+    let [location,setLocation]=useState("");
+    let [phone,setPhone]=useState("");
+
+    function edit (id){
+        setproductdata(nproductdata.filter((ele)=>{
+            if(ele.name === id){
+                ele.name = name;
+                ele.email = email;
+                ele.phone = phone;
+                ele.location = location;      
+            }
+            return 1;
+        }))
+    }
+
+    function fname(e){
+        setName(e.target.value)
+    }
+    function femail(e){
+        setEmail(e.target.value)
+    }
+    function fLocation(e){
+        setLocation(e.target.value)
+    }
+    function fphone(e){
+        setPhone(e.target.value)
+    }
     return (
         <>
+            <div>
+                <form className='flex justify-center border-2'>
+                    <div>
+                        <input type='text' placeholder='Name' value={name} onChange={fname} className='border-2 border-black'/><br/><br/>
+                        <input type='email' placeholder='Email' value={email} onChange={femail} className='border-2 border-black'/><br/><br/>
+                        <input type='text' placeholder='Location' value={location} onChange={fLocation} className='border-2 border-black'/><br/><br/>
+                        <input type='tel' placeholder='Phone' value={phone} onChange={fphone} className='border-2 border-black'/><br/><br/>
+                    </div> 
+                </form>
+            </div>
+
             <div className='w-full bg-gray-50 h-auto flex'>
                 <h1 className='ps-24 pt-2 font-bold text-xl w-1/5'>Contacts</h1>
                 <div className=' w-full h-12 pt-2 justify-end flex'>
@@ -35,34 +73,29 @@ const Product = () => {
                         <th>Phone </th>
                         <th>Actions</th>
                     </tr>
-
                 </thead>
                 <tbody>
                     {
-                        productalias.map((ProductListing) => {
+                        nproductdata.map((productalias) => {
                             return (
                                 <tr className='text-center h-12 border border-slate-400 text-gray-500 text-sm font-semibold'>
-                                    <td className='h-12 flex'><span className='ms-20 my-auto'><img src={ProductListing.image} alt='' className='rounded-full h-8 w-8'></img></span><span className='pt-3 ms-3'>{ProductListing.name}</span></td>
-                                    <td className=''>{ProductListing.email}</td>
-                                    <td className=''>{ProductListing.location}</td>
-                                    <td className=''>{ProductListing.phone}</td>
+                                    <td className='h-12 flex'><span className='ms-20 my-auto'></span><span className='pt-3 ms-3'>{productalias.name}</span></td>
+                                    <td className=''>{productalias.email}</td>
+                                    <td className=''>{productalias.location}</td>
+                                    <td className=''>{productalias.phone}</td>
                                     <td >
                                         <div className='flex justify-center'>
-                                            <button className='border border-blue-500 rounded-lg items-center h-6 w-auto px-2 text-blue-700 font-semibold' onClick={()=>handleClick}>Edit</button>
-                                            <button className='border border-red-500 rounded-lg  my-auto h-6 w-auto  ms-5 px-2 text-red-700 font-semibold' onClick={() => handleRemove(ProductListing.id)}>Delete</button>
+                                            <button className='border border-blue-500 rounded-lg items-center h-6 w-auto px-2 text-blue-700 font-semibold' onClick={()=>edit(productalias.name)}>Edit</button>
+                                            <button className='border border-red-500 rounded-lg  my-auto h-6 w-auto  ms-5 px-2 text-red-700 font-semibold' onClick={() => handleRemove(productalias.id)}>Delete</button>
                                         </div>
                                     </td>
                                 </tr>
-
                             )
                         })
                     }
-
                 </tbody>
             </table>
         </>
-
-
     )
 }
 
